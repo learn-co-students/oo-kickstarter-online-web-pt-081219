216c47b9f85a9dd598ba893b4db9af5c8ddc9586
wifelette <leahsilber@gmail.com>
2019-09-24 14:16:59 -0700

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,3 @@
-require 'pry-byebug'
-
 class Project
   attr_reader :title, :backers
 
@@ -10,7 +8,6 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    # backer.backed_projects << self
     unless backer.backed_projects.include?(self)
       backer.backed_projects << self
     end
