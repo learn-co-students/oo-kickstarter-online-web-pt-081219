960d57a7a25f64e5056b9de56acf0293547ab018
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-26 19:35:45 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,5 @@
+class backer
+  
+  attr_reader :name
+  @backed_projects = backed_projects
+  add_backer()
---
+++ lib/project.rb
@@ -0,0 +1,4 @@
+class.project
+
+attr_reader :backer
+@backed_projects = ("backed_projects")
