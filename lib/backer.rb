2b7ec0a59c8fadbf8d8f547b5edfe27f6df68541
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-09-30 01:23:52 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Backer
+  
+attr_accessor :name, :backed_projects 
+#attr_reader :name
+
+def initialize(name)
+  @name = name 
+  @backed_projects = []
+end 
+def back_project(project)
+  @project = project
+  @backed_projects << project  
+  end
+  
+end 
+
+
+
+
---
+++ lib/project.rb
@@ -0,0 +1,12 @@
+class Project 
+  attr_accessor :name, :backers  
+  attr_reader :title 
+  
+def initialize(title)
+  @title = title
+  @backers = []
+  end
+  
+def back_projects()
+  end 
+end 
